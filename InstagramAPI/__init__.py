db0fa07736cb27ad450c4ebbd708b9a906b4be34
Julian O <git@somethinkodd.com>
2017-06-28 04:18:31 +1000

    Remove some apparent testing code, right in the middle of the login script!
    Prepare for exception handling introduction.

--- InstagramAPI/__init__.py
+++ InstagramAPI/__init__.py
@@ -37,6 +37,17 @@ except:  # imageio.core.fetching.NeedDownloadError
 
 
 class InstagramAPI:
+
+    """
+    
+        General Note: Methods may raise exceptions from the requests module, including:
+            requests.ConnectionError
+            requests.ConnectTimeout
+            
+        Unlike earlier versions, they may also raise requests.HTTPError rather than returning False.
+    
+    """
+
     API_URL = 'https://i.instagram.com/api/v1/'
     DEVICE_SETTINTS = {
         'manufacturer': 'Xiaomi',
@@ -49,6 +60,7 @@ class InstagramAPI:
     EXPERIMENTS = 'ig_android_progressive_jpeg,ig_creation_growth_holdout,ig_android_report_and_hide,ig_android_new_browser,ig_android_enable_share_to_whatsapp,ig_android_direct_drawing_in_quick_cam_universe,ig_android_huawei_app_badging,ig_android_universe_video_production,ig_android_asus_app_badging,ig_android_direct_plus_button,ig_android_ads_heatmap_overlay_universe,ig_android_http_stack_experiment_2016,ig_android_infinite_scrolling,ig_fbns_blocked,ig_android_white_out_universe,ig_android_full_people_card_in_user_list,ig_android_post_auto_retry_v7_21,ig_fbns_push,ig_android_feed_pill,ig_android_profile_link_iab,ig_explore_v3_us_holdout,ig_android_histogram_reporter,ig_android_anrwatchdog,ig_android_search_client_matching,ig_android_high_res_upload_2,ig_android_new_browser_pre_kitkat,ig_android_2fac,ig_android_grid_video_icon,ig_android_white_camera_universe,ig_android_disable_chroma_subsampling,ig_android_share_spinner,ig_android_explore_people_feed_icon,ig_explore_v3_android_universe,ig_android_media_favorites,ig_android_nux_holdout,ig_android_search_null_state,ig_android_react_native_notification_setting,ig_android_ads_indicator_change_universe,ig_android_video_loading_behavior,ig_android_black_camera_tab,liger_instagram_android_univ,ig_explore_v3_internal,ig_android_direct_emoji_picker,ig_android_prefetch_explore_delay_time,ig_android_business_insights_qe,ig_android_direct_media_size,ig_android_enable_client_share,ig_android_promoted_posts,ig_android_app_badging_holdout,ig_android_ads_cta_universe,ig_android_mini_inbox_2,ig_android_feed_reshare_button_nux,ig_android_boomerang_feed_attribution,ig_android_fbinvite_qe,ig_fbns_shared,ig_android_direct_full_width_media,ig_android_hscroll_profile_chaining,ig_android_feed_unit_footer,ig_android_media_tighten_space,ig_android_private_follow_request,ig_android_inline_gallery_backoff_hours_universe,ig_android_direct_thread_ui_rewrite,ig_android_rendering_controls,ig_android_ads_full_width_cta_universe,ig_video_max_duration_qe_preuniverse,ig_android_prefetch_explore_expire_time,ig_timestamp_public_test,ig_android_profile,ig_android_dv2_consistent_http_realtime_response,ig_android_enable_share_to_messenger,ig_explore_v3,ig_ranking_following,ig_android_pending_request_search_bar,ig_android_feed_ufi_redesign,ig_android_video_pause_logging_fix,ig_android_default_folder_to_camera,ig_android_video_stitching_7_23,ig_android_profanity_filter,ig_android_business_profile_qe,ig_android_search,ig_android_boomerang_entry,ig_android_inline_gallery_universe,ig_android_ads_overlay_design_universe,ig_android_options_app_invite,ig_android_view_count_decouple_likes_universe,ig_android_periodic_analytics_upload_v2,ig_android_feed_unit_hscroll_auto_advance,ig_peek_profile_photo_universe,ig_android_ads_holdout_universe,ig_android_prefetch_explore,ig_android_direct_bubble_icon,ig_video_use_sve_universe,ig_android_inline_gallery_no_backoff_on_launch_universe,ig_android_image_cache_multi_queue,ig_android_camera_nux,ig_android_immersive_viewer,ig_android_dense_feed_unit_cards,ig_android_sqlite_dev,ig_android_exoplayer,ig_android_add_to_last_post,ig_android_direct_public_threads,ig_android_prefetch_venue_in_composer,ig_android_bigger_share_button,ig_android_dv2_realtime_private_share,ig_android_non_square_first,ig_android_video_interleaved_v2,ig_android_follow_search_bar,ig_android_last_edits,ig_android_video_download_logging,ig_android_ads_loop_count_universe,ig_android_swipeable_filters_blacklist,ig_android_boomerang_layout_white_out_universe,ig_android_ads_carousel_multi_row_universe,ig_android_mentions_invite_v2,ig_android_direct_mention_qe,ig_android_following_follower_social_context'
     SIG_KEY_VERSION = '4'
 
+    # TODO: Make all of these have an underscore prefix.
     # username            # Instagram username
     # password            # Instagram password
     # debug               # Debug
@@ -95,12 +107,6 @@ class InstagramAPI:
                     self.rank_token = "%s_%s" % (self.username_id, self.uuid)
                     self.token = self.LastResponse.cookies["csrftoken"]
 
-                    self.syncFeatures()
-                    self.autoCompleteUserList()
-                    self.timelineFeed()
-                    self.getv2Inbox()
-                    self.getRecentActivity()
-                    print("Login success!\n")
                     return True
 
     def syncFeatures(self):
@@ -687,13 +693,6 @@ class InstagramAPI:
         return 'android-' + m.hexdigest()[:16]
 
     def generateUUID(self, type_):
-        # according to https://github.com/LevPasha/Instagram-API-python/pull/16/files#r77118894
-        # uuid = '%04x%04x-%04x-%04x-%04x-%04x%04x%04x' % (random.randint(0, 0xffff),
-        #    random.randint(0, 0xffff), random.randint(0, 0xffff),
-        #    random.randint(0, 0x0fff) | 0x4000,
-        #    random.randint(0, 0x3fff) | 0x8000,
-        #    random.randint(0, 0xffff), random.randint(0, 0xffff),
-        #    random.randint(0, 0xffff))
         generated_uuid = str(uuid.uuid4())
         if type_:
             return generated_uuid
@@ -739,6 +738,8 @@ class InstagramAPI:
         else:  # GET
             response = self.s.get(self.API_URL + endpoint)  # , verify=False
 
+        # response.raise_for_status()
+
         if response.status_code == 200:
             self.LastResponse = response
             self.LastJson = json.loads(response.text)
@@ -754,7 +755,8 @@ class InstagramAPI:
             except:
                 pass
             return False
-            
+
+    # TODO: Replace with iterator.
     def getTotalFollowers(self, usernameId):
         followers = []
         next_max_id = ''
@@ -765,6 +767,7 @@ class InstagramAPI:
             for item in temp["users"]:
                 followers.append(item)
 
+            # TODO: Replace with check for next_max_id = None
             if not temp["big_list"]:
                 return followers            
             next_max_id = temp["next_max_id"]         

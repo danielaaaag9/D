62c8eb441bf86393989ca69020bebf72a5a27508
Julian O <git@somethinkodd.com>
2017-06-28 05:07:01 +1000

    Unit test login.
    * Move tests to use unittest.
    * Add test for logging in and out.
    * Tests reveal there is no exception - make exception subclass.
    * Tests reveal calls fail PRIOR to AuthenticationError - put defaults in for username_id, etc.
    * Tests reveal logout doesn't record that it is logged out - update self.isLoggedIn
    
    Also narrow other exceptions clauses.

--- InstagramAPI/__init__.py
+++ InstagramAPI/__init__.py
@@ -24,7 +24,7 @@ if sys.version_info.major == 3:
 
 try:
     from ImageUtils import getImageSize
-except:
+except ImportError:
     # Issue 159, python3 import fix
     from .ImageUtils import getImageSize
 
@@ -37,7 +37,7 @@ except:  # imageio.core.fetching.NeedDownloadError
 
 try:
     import credentials
-except:
+except ImportError:
     pass  # Only here because of the weird __init__.py structure.
 
 
@@ -65,6 +65,9 @@ class InstagramAPI:
     EXPERIMENTS = 'ig_android_progressive_jpeg,ig_creation_growth_holdout,ig_android_report_and_hide,ig_android_new_browser,ig_android_enable_share_to_whatsapp,ig_android_direct_drawing_in_quick_cam_universe,ig_android_huawei_app_badging,ig_android_universe_video_production,ig_android_asus_app_badging,ig_android_direct_plus_button,ig_android_ads_heatmap_overlay_universe,ig_android_http_stack_experiment_2016,ig_android_infinite_scrolling,ig_fbns_blocked,ig_android_white_out_universe,ig_android_full_people_card_in_user_list,ig_android_post_auto_retry_v7_21,ig_fbns_push,ig_android_feed_pill,ig_android_profile_link_iab,ig_explore_v3_us_holdout,ig_android_histogram_reporter,ig_android_anrwatchdog,ig_android_search_client_matching,ig_android_high_res_upload_2,ig_android_new_browser_pre_kitkat,ig_android_2fac,ig_android_grid_video_icon,ig_android_white_camera_universe,ig_android_disable_chroma_subsampling,ig_android_share_spinner,ig_android_explore_people_feed_icon,ig_explore_v3_android_universe,ig_android_media_favorites,ig_android_nux_holdout,ig_android_search_null_state,ig_android_react_native_notification_setting,ig_android_ads_indicator_change_universe,ig_android_video_loading_behavior,ig_android_black_camera_tab,liger_instagram_android_univ,ig_explore_v3_internal,ig_android_direct_emoji_picker,ig_android_prefetch_explore_delay_time,ig_android_business_insights_qe,ig_android_direct_media_size,ig_android_enable_client_share,ig_android_promoted_posts,ig_android_app_badging_holdout,ig_android_ads_cta_universe,ig_android_mini_inbox_2,ig_android_feed_reshare_button_nux,ig_android_boomerang_feed_attribution,ig_android_fbinvite_qe,ig_fbns_shared,ig_android_direct_full_width_media,ig_android_hscroll_profile_chaining,ig_android_feed_unit_footer,ig_android_media_tighten_space,ig_android_private_follow_request,ig_android_inline_gallery_backoff_hours_universe,ig_android_direct_thread_ui_rewrite,ig_android_rendering_controls,ig_android_ads_full_width_cta_universe,ig_video_max_duration_qe_preuniverse,ig_android_prefetch_explore_expire_time,ig_timestamp_public_test,ig_android_profile,ig_android_dv2_consistent_http_realtime_response,ig_android_enable_share_to_messenger,ig_explore_v3,ig_ranking_following,ig_android_pending_request_search_bar,ig_android_feed_ufi_redesign,ig_android_video_pause_logging_fix,ig_android_default_folder_to_camera,ig_android_video_stitching_7_23,ig_android_profanity_filter,ig_android_business_profile_qe,ig_android_search,ig_android_boomerang_entry,ig_android_inline_gallery_universe,ig_android_ads_overlay_design_universe,ig_android_options_app_invite,ig_android_view_count_decouple_likes_universe,ig_android_periodic_analytics_upload_v2,ig_android_feed_unit_hscroll_auto_advance,ig_peek_profile_photo_universe,ig_android_ads_holdout_universe,ig_android_prefetch_explore,ig_android_direct_bubble_icon,ig_video_use_sve_universe,ig_android_inline_gallery_no_backoff_on_launch_universe,ig_android_image_cache_multi_queue,ig_android_camera_nux,ig_android_immersive_viewer,ig_android_dense_feed_unit_cards,ig_android_sqlite_dev,ig_android_exoplayer,ig_android_add_to_last_post,ig_android_direct_public_threads,ig_android_prefetch_venue_in_composer,ig_android_bigger_share_button,ig_android_dv2_realtime_private_share,ig_android_non_square_first,ig_android_video_interleaved_v2,ig_android_follow_search_bar,ig_android_last_edits,ig_android_video_download_logging,ig_android_ads_loop_count_universe,ig_android_swipeable_filters_blacklist,ig_android_boomerang_layout_white_out_universe,ig_android_ads_carousel_multi_row_universe,ig_android_mentions_invite_v2,ig_android_direct_mention_qe,ig_android_following_follower_social_context'
     SIG_KEY_VERSION = '4'
 
+    class AuthenticationError(RuntimeError):
+        pass
+
     # TODO: Make all of these have an underscore prefix.
     # username            # Instagram username
     # password            # Instagram password
@@ -78,6 +81,10 @@ class InstagramAPI:
     # IGDataPath          # Data storage path
 
     def __init__(self, username, password, debug=False, IGDataPath=None):
+        self.username_id = ''
+        self.rank_token = ''
+        self.token = ''
+
         m = hashlib.md5()
         m.update(username.encode('utf-8') + password.encode('utf-8'))
         self.device_id = self.generateDeviceId(m.hexdigest())
@@ -144,8 +151,10 @@ class InstagramAPI:
         return self._sendrequest('qe/expose/', self.generateSignature(data))
 
     def logout(self):
-        logout = self._sendrequest('accounts/logout/')
-        # TODO: Should return a value, and set self.isLoggedIn as a side-effect.
+        try:
+            return self._sendrequest('accounts/logout/')
+        finally:
+            self.isLoggedIn = False
 
     def uploadPhoto(self, photo, caption=None, upload_id=None):
         if upload_id is None:
@@ -727,8 +736,13 @@ class InstagramAPI:
         return body
     
     def _sendrequest(self, endpoint, post=None, login=False):
+
+        # login parameter indicates this is a call to login, so no need to check we are logged in.
+        # Otherwise...
+
         if not self.isLoggedIn and not login:
-            raise Exception("Not logged in!\n")
+            raise InstagramAPI.AuthenticationError("Not logged in.")
+
         self.s.headers.update({
             'Connection': 'close',
             'Accept': '*/*',
--- test.py
+++ test.py
@@ -1,13 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Use text editor to edit the script and type in valid Instagram username/password
 
-from InstagramAPI import InstagramAPI
+# See ReadMe for preparation instructions for credentials.
 
-InstagramAPI = InstagramAPI("login", "password")
-InstagramAPI.login() # login
-InstagramAPI.tagFeed("cat") # get media list by tag #cat
-media_id = InstagramAPI.LastJson # last response JSON
-InstagramAPI.like(media_id["ranked_items"][0]["pk"]) # like first media
-InstagramAPI.getUserFollowers(media_id["ranked_items"][0]["user"]["pk"]) # get first media owner followers
+import unittest
+from InstagramAPI import InstagramAPI, credentials
+
+"""
+    WARNING: These tests may affect your account. Use a test account.
+"""
+
+
+class InstagramAPITests(unittest.TestCase):
+
+    def test_login(self):
+        api = InstagramAPI(username=credentials.USERNAME, password=credentials.PASSWORD)
+
+        # Trying to access something before you log in fails with Authentication error.
+        with self.assertRaises(InstagramAPI.AuthenticationError):
+            api.getSelfUserFollowers(None)
+        with self.assertRaises(InstagramAPI.AuthenticationError):
+            api.logout()
+
+        # Logging in raises no exception.
+        # Not checking the return value, because expect that to disappear in the future.
+        api.login()
+
+        # Once logged in, don't get authentication errors.
+
+        api.getSelfUserFollowers()
+        api.logout()
+
+        # Logging out means you get authentication errors again.
+        with self.assertRaises(InstagramAPI.AuthenticationError):
+            api.logout()
+
+        # You can repeat the login process.
+        api.login()
+        api.getSelfUserFollowers()
+        api.logout()
+
+    def test_like_latest_cat(self):
+        api = InstagramAPI(username=credentials.USERNAME, password=credentials.PASSWORD)
+        api.login()
+        api.tagFeed("cat") # get media list by tag #cat
+        media_id = api.LastJson # last response JSON
+        api.like(media_id["ranked_items"][0]["pk"]) # like first media
+        api.getUserFollowers(media_id["ranked_items"][0]["user"]["pk"]) # get first media owner followers
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)

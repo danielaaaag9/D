cbac4fea5e9e81511336102c6acad1e4f9401945
Julian O <git@somethinkodd.com>
2017-06-28 00:55:16 +1000

    Reformat code to get closer to PEP8 compliance.
    * Tried to avoid any actual changes to code.
      * Avoided changing identifier names, so clients don't break.
    * Marked several outstanding issues and bugs with TODO.
    * Removed unused imports.
    * Made generateUploadId static (had no self param).
    * Changed "X == False" to "not X" in a couple of places.
    * Changed a couple of example captions to English.
    * Copy-edited description of project.

--- examples/user_followings.py
+++ examples/user_followings.py
@@ -1,29 +1,30 @@
 from InstagramAPI import InstagramAPI
-import time
-from datetime import datetime
 
 username = ''
 pwd = ''
-user_id  = ''
+user_id = ''
 
-API = InstagramAPI(username,pwd)
+API = InstagramAPI(username, pwd)
 API.login()
 
 API.getUsernameInfo(user_id)
-API.LastJson
-following   = []
+
+following = []
 next_max_id = True
 while next_max_id:
-    print next_max_id
-    #first iteration hack
-    if next_max_id == True: next_max_id=''
-    _ = API.getUserFollowings(user_id,maxid=next_max_id)
-    following.extend ( API.LastJson.get('users',[]))
-    next_max_id = API.LastJson.get('next_max_id','')
+    print(next_max_id)
+    # first iteration hack
+    if next_max_id:
+        next_max_id = ''
+    _ = API.getUserFollowings(user_id, maxid=next_max_id)
+    following.extend(API.LastJson.get('users', []))
+    next_max_id = API.LastJson.get('next_max_id', '')
 
+
+# TODO: These statements have no effect.
 len(following)
 unique_following = {
-    f['pk'] : f
+    f['pk']: f
     for f in following
 }
 len(unique_following)

8f5d176797f3a2092d6d0795835fae14e41f437f
Julian O <git@somethinkodd.com>
2017-06-29 12:46:50 +1000

    Use credentials in example.

--- examples/direct_share.py
+++ examples/direct_share.py
@@ -1,6 +1,5 @@
-from InstagramAPI import InstagramAPI
-user, pwd = '', ''                          # your credentials
-InstagramAPI = InstagramAPI(user, pwd)
+from InstagramAPI import InstagramAPI, credentials
+InstagramAPI = InstagramAPI(credentials.USERNAME, credentials.PASSWORD)
 InstagramAPI.login()                        # login
 mediaId = '1469246128528859784_1520706701'  # a media_id
 recipients = []                             # array of user_ids. They can be strings or ints

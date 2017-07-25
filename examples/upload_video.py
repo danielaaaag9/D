200c7bf46dcedcf81e77d6d3060c837997bc7f1e
Rattapoom <rm.manaosoftware@outlook.com>
2017-07-25 17:51:25 +0700

    fix example

--- examples/upload_video.py
+++ examples/upload_video.py
@@ -5,14 +5,19 @@
 
 from InstagramAPI import InstagramAPI, credentials
 import urllib
+import sys
+
 video_url = 'https://instagram.fmad3-2.fna.fbcdn.net/t50.2886-16/17157217_1660580944235536_866261046376005632_n.mp4'  # a valid instagram video
 video_local_path = video_url.split("/")[-1]
 thumbnail_url = "https://instagram.fmad3-2.fna.fbcdn.net/t51.2885-15/e15/17075853_1759410394387536_3927726791665385472_n.jpg"
 thumbnail_local_path = thumbnail_url.split("/")[-1]
 
-# TODO: These only work in Python 2.
-urllib.urlretrieve(video_url, video_local_path)
-urllib.urlretrieve(thumbnail_url, thumbnail_local_path)
+if sys.version_info.major == 3:
+    urllib.request.urlretrieve(video_url, video_local_path)
+    urllib.request.urlretrieve(thumbnail_url, thumbnail_local_path)
+else:
+    urllib.urlretrieve(video_url, video_local_path)
+    urllib.urlretrieve(thumbnail_url, thumbnail_local_path)
 
 API = InstagramAPI(credentials.USERNAME, credentials.PASSWORD)
 
